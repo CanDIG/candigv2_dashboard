98164e92358226549b791ae51f7248e4e590d564
Felipe <fcoralsasso@bcgsc.ca>
2020-07-09 10:37:39 -0700

    Using BASEURL for APIInfo

--- src/views/APIInfo.js
+++ src/views/APIInfo.js
@@ -5,19 +5,15 @@ import "swagger-ui-react/swagger-ui.css";
 
 // reactstrap components
 import {
-  //   Button,
   Card,
-  //   CardHeader,
   CardBody,
-  //   CardFooter,
-  //   CardTitle,
-  //   FormGroup,
-  //   Form,
-  //   Input,
   Row,
   Col,
   Table,
 } from "reactstrap";
+
+// Consts
+import BASE_URL from "../constants/constants.js"
 
 class APIInfo extends React.Component {
   render() {
@@ -29,7 +25,7 @@ class APIInfo extends React.Component {
               <Card>
                 <CardBody className="all-icons">
                   <Table responsive>
-                    <SwaggerUI url="http://ga4ghdev01.bcgsc.ca:20127/static/api.yaml" />
+                    <SwaggerUI url={BASE_URL + "/static/api.yaml"} />
                   </Table>
                 </CardBody>
               </Card>
