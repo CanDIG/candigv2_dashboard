3743ebf7d422d6569925eeab9f3f4cafa12aa4c3
Felipe <fcoralsasso@bcgsc.ca>
2020-07-10 15:10:21 -0700

    Added comment to constants.js file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,8 @@
+/*
+ * All the constants should go on this file
+ */
+
+ // API URL where the Dashboard get all the data
 const BASE_URL = "http://ga4ghdev01.bcgsc.ca:20127";
 
-export default BASE_URL
+export default BASE_URL;
