a36ef429ba18245a257abe53789ad1b4cf3d305d
Felipe <fcoralsasso@bcgsc.ca>
2020-07-10 15:14:37 -0700

    Removed copyright from Overview.js

--- src/views/Overview.js
+++ src/views/Overview.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 // reactstrap components
 import { Card, CardBody, CardTitle, Row, Col } from "reactstrap";
@@ -44,8 +26,8 @@ class Dashboard extends React.Component {
   }
 
   componentDidMount() {
-    if(this.props.datasetId){
-      this.fetchData(this.props.datasetId); 
+    if (this.props.datasetId) {
+      this.fetchData(this.props.datasetId);
     }
   }
 
