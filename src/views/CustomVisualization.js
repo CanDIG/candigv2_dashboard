b42a27f2725860f338ed29ad704b25d76b15813e
Felipe <fcoralsasso@bcgsc.ca>
2020-08-05 12:05:01 -0700

    Changed CustomVisualization  to use hooks

--- src/views/CustomVisualization.js
+++ src/views/CustomVisualization.js
@@ -4,31 +4,29 @@ import { Row, Col, Container } from "reactstrap";
 
 import CustomVisualizationDropDown from "../components/Dropdowns/CustomVisualizationDropDown.js";
 
-class CustomVisualization extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="content">
-          <Container>
-            <Row>
-              <Col sm="12" xs="12" md="12" lg="6" xl="6">
-                <CustomVisualizationDropDown
-                  datasetId={this.props.datasetId}
-                  datasetName={this.props.datasetName}
-                />
-              </Col>
-              <Col sm="12" xs="12" md="12" lg="6" xl="6">
-                <CustomVisualizationDropDown
-                  datasetId={this.props.datasetId}
-                  datasetName={this.props.datasetName}
-                />
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </>
-    );
-  }
+function CustomVisualization({ datasetId, datasetName }) {
+  return (
+    <>
+      <div className="content">
+        <Container>
+          <Row>
+            <Col sm="12" xs="12" md="12" lg="6" xl="6">
+              <CustomVisualizationDropDown
+                datasetId={datasetId}
+                datasetName={datasetName}
+              />
+            </Col>
+            <Col sm="12" xs="12" md="12" lg="6" xl="6">
+              <CustomVisualizationDropDown
+                datasetId={datasetId}
+                datasetName={datasetName}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
 
 export default CustomVisualization;
