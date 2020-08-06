b1baf6461773487aa00125da00a32a347fefd79b
Felipe <fcoralsasso@bcgsc.ca>
2020-08-06 10:18:49 -0700

    Changed APIInfo to use hooks

--- src/views/APIInfo.js
+++ src/views/APIInfo.js
@@ -4,37 +4,29 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 // reactstrap components
-import {
-  Card,
-  CardBody,
-  Row,
-  Col,
-  Table,
-} from "reactstrap";
+import { Card, CardBody, Row, Col, Table } from "reactstrap";
 
 // Consts
-import BASE_URL from "../constants/constants.js"
+import BASE_URL from "../constants/constants.js";
 
-class APIInfo extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="content">
-          <Row>
-            <Col md="12">
-              <Card>
-                <CardBody className="all-icons">
-                  <Table responsive>
-                    <SwaggerUI url={BASE_URL + "/static/api.yaml"} />
-                  </Table>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </>
-    );
-  }
+function APIInfo() {
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col md="12">
+            <Card>
+              <CardBody className="all-icons">
+                <Table responsive>
+                  <SwaggerUI url={BASE_URL + "/static/api.yaml"} />
+                </Table>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 }
 
 export default APIInfo;
