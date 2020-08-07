302ece302a56e60bc0c9260b95cb29bf4abd8d3f
Jimmy Li <lihaoyuan1996@gmail.com>
2020-08-07 13:55:16 -0700

    use hooks

--- src/views/GwasBrowser.js
+++ src/views/GwasBrowser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Row, Input } from "reactstrap";
 
 // TODO: Importing from igv.esm.min.js is not working, but it works fine in the standalone test
@@ -8,119 +8,79 @@ import igv from 'igv/dist/igv.esm.js'
 import BASE_URL from "../constants/constants.js"
 
 
-class GwasBrowser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedGwasName: ""
-        }
-        this.igvBrowserRef = React.createRef();
-        this.gwasFileList = {};
-        this.igvOptions =  {
-          genome: 'hg38',
-          tracks: [
-            {
-                type: "gwas",
-                format: "gwas",
-                name: "",
-                url: "",
-                indexed: false,
-                height: 300,
-                columns: {
-                    chromosome: 1,
-                    position: 2,
-                    value: 5
-                },
-                dotSize: 1.5
-            }
-        ]}
-    }
-    
-    /**
-     * Invoked when this.state.selectedGwasName is updated
-     */
-    componentDidUpdate(prevState) {
-      //TODO: Use igv.removeBrowser to remove existing browser instance
-
-        if (this.state.selectedGwasName !== prevState.selectedGwasName) {
-            this.igvOptions["tracks"][0]["name"] = this.state.selectedGwasName;
-            this.igvOptions["tracks"][0]["url"] = BASE_URL + this.gwasFileList[this.state.selectedGwasName]
-
-            for (let i = 0; i < this.igvBrowserRef.current.childNodes.length; i++){
-              let curr = this.igvBrowserRef.current.childNodes[i];
-              if (curr.className === "igv-root-div") {
-                curr.remove();
-              }
-            }
-            this.igvBrowserRef.current.remove()
-            igv.createBrowser(this.igvBrowserRef.current, this.igvOptions);
-        }
-    }
-
-    /**
-     * Invoked when a different option of dropdown is selected.
-     */
-    handleGwasInputClick = (e) => {
-        this.setState({ selectedGwasName: e.target.value});
-    };
-
-    /*
-    * Create dropdown for gwas data files
-    * This method is invoked in render()
-    */
-    buildGwasList() {
-      // This should be replaced by an XHR request that retrieves a list of files from an API Server
-      var mock_data = {
-        "ANA_A2_V2_filtered": "/static/COVID19_HGI_ANA_A2_V2_20200701.txt.gz_1.0E-5.txt",
-        "ANA_B1_V2": "/static/COVID19_HGI_ANA_B1_V2.gwas", 
-        "ANA_B2_V2": "/static/minimal.gwas.1e-2.txt",
-        "ANA_C1_V2_filtered": "/static/COVID19_HGI_ANA_C1_V2_20200701.txt.gz_1.0E-5.txt",
-        "ANA_D1_V2_filtered": "/static/COVID19_HGI_ANA_D1_V2_20200701.txt.gz_1.0E-5.txt"
+function GwasBrowser() {
+  /***
+   * A functional component that renders an IGV.js instance
+   * 
+   */
+  const [selectedGwasName, setSelectedGwasName] = useState("");
+  const igvBrowser = useRef(null);
+  const igvOptions = {
+    genome: 'hg38',
+    tracks: [
+      {
+          type: "gwas",
+          format: "gwas",
+          name: "",
+          url: "",
+          indexed: false,
+          height: 300,
+          columns: {
+              chromosome: 1,
+              position: 2,
+              value: 5
+          },
+          dotSize: 1.5
       }
+  ]};
 
-      this.gwasFileList = mock_data;
-
-      return Object.keys(mock_data).map((x) => {
-          return (
-            <option key={x} value={x}>
-              {x}
-            </option>
-          );
-        });
-    }
-
-
-    /**
-     * Invoked when render() completes
-     * By default, load the first gwas file available.
-     */
-    componentDidMount() {
-      // Use the first gwas file name as the initial state
-      this.setState({ selectedGwasName: Object.keys(this.gwasFileList)[0] });
-      this.igvOptions["tracks"][0]["name"] = this.state.selectedGwasName;
-      this.igvOptions["tracks"][0]["url"] = BASE_URL + this.gwasFileList[this.state.selectedGwasName]
-
-    }
-
-    render() {
-      return (
-        <>
-            <div className="content">
-                <Row>
-                    <Input onChange={this.handleGwasInputClick} type="select">
-                        { this.buildGwasList() }
-                    </Input>
-                </Row>
-                <div ref={this.igvBrowserRef}></div>
-            </div>
-        </>
-      );
-    }
+  const mock_data = {
+    "ANA_A2_V2_filtered": "/static/COVID19_HGI_ANA_A2_V2_20200701.txt.gz_1.0E-5.txt",
+    "ANA_B1_V2": "/static/COVID19_HGI_ANA_B1_V2.gwas", 
+    "ANA_B2_V2": "/static/minimal.gwas.1e-2.txt",
+    "ANA_C1_V2_filtered": "/static/COVID19_HGI_ANA_C1_V2_20200701.txt.gz_1.0E-5.txt",
+    "ANA_D1_V2_filtered": "/static/COVID19_HGI_ANA_D1_V2_20200701.txt.gz_1.0E-5.txt"
   }
 
-//   ReactDOM.render(
-//     <GwasBrowser />,
-//     document.getElementById('root')
-//   );
+  const gwasList = Object.keys(mock_data).map((x) => {
+    return (
+      <option key={x} value={x}>
+        {x}
+      </option>
+    );
+  });
 
-  export default GwasBrowser;
+  useEffect(() => {
+
+    // Remove an existing browser instance
+    if (igv.getBrowser() !== undefined) {
+      igv.removeBrowser(igv.getBrowser());
+    }
+
+    // Do not create new browser instance when nothing is selected
+    if (selectedGwasName != "") {
+      igvOptions["tracks"][0]["name"] = selectedGwasName;
+      igvOptions["tracks"][0]["url"] = BASE_URL + mock_data[selectedGwasName];
+      igv.createBrowser(igvBrowser.current, igvOptions);
+    }
+  })
+
+  return (
+    <>
+        <div className="content">
+            <Row>
+                <Input 
+                  onChange={(e) => {
+                    setSelectedGwasName(e.currentTarget.value);
+                  }}
+                  type="select">
+                  { gwasList }
+                </Input>
+            </Row>
+            <div ref={igvBrowser}></div>
+        </div>
+    </>
+  );
+}
+
+export default GwasBrowser;
