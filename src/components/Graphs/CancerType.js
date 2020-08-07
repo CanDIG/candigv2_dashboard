d0ee8695311b0d78639b9709e0d4dcd727f33fb0
Felipe <fcoralsasso@bcgsc.ca>
2020-08-07 10:17:42 -0700

    Changed CancerType to ue Hooks

--- src/components/Graphs/CancerType.js
+++ src/components/Graphs/CancerType.js
@@ -1,114 +1,98 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 // Consts
 import BASE_URL from "../../constants/constants.js";
 
-class CancerType extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chartOptions: {
-        credits: {
-          enabled: false,
-        },
-        chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          plotShadow: false,
-          type: "pie",
-        },
-        title: {
-          text: "Cancer type",
-        },
+// Hook
+// Used to keep the previous value of a state or prop
+function usePrevious(value) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+/*
+ * Pie graph component for type of cancer data
+ * @param {string} datasetId
+ */
+
+function CancerType({ datasetId }) {
+  const [chartOptions, setChartOptions] = useState({
+    credits: {
+      enabled: false,
+    },
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: "pie",
+    },
+    title: {
+      text: "Cancer type",
+    },
+    series: [
+      {
+        colorByPoint: true,
+        showInLegend: false,
+        data: [],
       },
-      datasetId: "",
-    };
-  }
+    ],
+  });
 
-  componentDidUpdate(prevProps) {
-    if (this.props.datasetId !== prevProps.datasetId) {
-      this.fetchData(this.props.datasetId);
-    }
-  }
+  const prevDatasetId = usePrevious(datasetId);
 
-  componentDidMount() {
-    if (this.props.datasetId) {
-      this.fetchData(this.props.datasetId);
-    }
-  }
-
-  fetchData(datasetId) {
-    fetch(BASE_URL + "/count", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        dataset_id: datasetId,
-        logic: {
-          id: "A",
-        },
-        components: [
-          {
+  useEffect(() => {
+    if (prevDatasetId !== datasetId && datasetId) {
+      fetch(BASE_URL + "/count", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          dataset_id: datasetId,
+          logic: {
             id: "A",
-            diagnoses: {},
           },
-        ],
-        results: [
-          {
-            table: "diagnoses",
-            fields: ["cancerType"],
-          },
-        ],
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const graphData = [];
-        if (data) {
-          let cancerType = data.results.diagnoses[0].cancerType;
+          components: [
+            {
+              id: "A",
+              diagnoses: {},
+            },
+          ],
+          results: [
+            {
+              table: "diagnoses",
+              fields: ["cancerType"],
+            },
+          ],
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const cancerType = data.results.diagnoses[0].cancerType;
+          const graphData = [];
           for (const name in cancerType) {
             graphData.push({ name: name, y: cancerType[name] });
           }
-        }
-        const chart = {
-          series: [
-            {
-              data: graphData,
-            },
-          ],
-        };
-        this.setState({ chartOptions: chart });
-      })
-      .catch((err) => {
-        const chart = {
-          chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-          },
-          title: {
-            text: "No data in pie chart",
-          },
-          series: [
-            {
-              type: "pie",
-              name: "Random data",
-              data: [],
-            },
-          ],
-        };
-        this.setState({ chartOptions: chart });
-      });
-  }
+          setChartOptions({
+            series: [
+              {
+                data: graphData,
+              },
+            ],
+          });
+        });
+    }
+  });
 
-  render() {
-    const chartOptions = this.state.chartOptions;
-    return (
-      <div>
-        <HighchartsReact highcharts={Highcharts} options={chartOptions} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+    </div>
+  );
 }
 
 export default CancerType;
