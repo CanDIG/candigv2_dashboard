8bb3eeb6329540de380d7d50c4005a92284de38a
Dashaylan Naidoo <dashaylan@gmail.com>
2020-08-11 12:38:14 -0700

    Move Styled components to own folder

--- src/views/MetadataTable.js
+++ src/views/MetadataTable.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { useState } from "react";
 import { 
   useTable,
@@ -26,8 +8,6 @@ import {
   useGroupBy,
   useExpanded,
 } from 'react-table'
-import styled from 'styled-components'
-import BASE_URL from 'constants/constants'
 import {
   GlobalFilter,
   DefaultColumnFilter,
@@ -53,75 +33,9 @@ import {
   
 } from "reactstrap";
 
-import MetaDropdown from "components/Dropdown/ClinMetadataDropdown.js"
-
-const Styles = styled.div`
-  padding: 1rem;
-  overflow: auto;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-
-  .pageCountBox {
-    margin-left: .3rem;
-    // padding: 10px 10px 10px 10px;
-  }
-
-  //Need to figure out how to do this for individual inputs
-
-  .form-group .input-group-prepend .input-group-text, 
-  .form-group .input-group-append .input-group-text, 
-  .input-group .input-group-prepend .input-group-text, 
-  .input-group .input-group-append .input-group-text {
-    padding: 10px 10px 10px 10px;
-  }
-
-  .pageCountOuter {
-    background-color: #e9ecef;
-  }
-
-  .goToPage {
-    background-color: #e9ecef;
-  }
-
-  .tablePageInput {
-    min-width: 0px;
-    width: 10%;
-    flex: none
-  }
-
-  .input-group .input-group-prepend .btn {
-    
-    margin-top: 0%;
-    margin-bottom: 0%;
-    margin-left: 0%;
-    margin-right: 0%;
-  
-  }
-`
-
+import BASE_URL from 'constants/constants';
+import MetaDropdown from "components/Dropdown/ClinMetadataDropdown.js";
+import Styles from "assets/css/StyledComponents/MetadataTableStyled.js";
 
 const IndeterminateButton = React.forwardRef(
   ({ indeterminate, ...rest }, ref) => {
