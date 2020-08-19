ac2603f47e23e3ea1e3f0b0b6927126783229ce2
Dashaylan Naidoo <dashaylan@gmail.com>
2020-08-19 14:44:11 -0700

    Clinical Metadata Table Integration (#31)
    * Add Table components uses react table hooks.
    
    * Add request with failure check
    
    * Working clinical metadata view
    
    * Add scrolling and column visibility
    
    * Add pagination instead of vertical scroll
    
    * Add column toggle controls
    
    * Add active state to column toggles
    
    * Add table sorting
    
    * Add global and column level filtering
    
    * Add basic aggregations
    
    * Fix column sorting to be dependant on pressing the column name. Removed unused imports.
    
    * Revert patients to smaller dataset size. Rename Table to MetadataTable.
    
    * Remove local testing files
    
    * Move Styled components to own folder
    
    * Add prop destructuring to TableApp, moved view to be above stock views
    
    * Add eslint, fixing style errors
    
    * Add functional component to generate pagination bar
    
    * Convert original metadata table code into three components
    
    * Change ternary expressions in ClinMetadataTable to use functions
    
    * Finish linting

---
+++ src/components/Dropdown/ClinMetadataDropdown.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+// Consts
+import { CLIN_METADATA } from 'constants/constants'
+
+class ClinMetadataDropdown extends React.Component {
+  constructor(props) {
+    super(props)
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false,
+      selectedMetadata: "Types"
+    }
+  }
+
+
+  toggle(event) {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  handleClick = (e) => {
+    const click = e.currentTarget;
+    this.setState({
+      selectedMetadata: click.textContent,
+    }, () => 
+      this.sendMetadata(click.textContent)
+    );
+
+  }
+
+  sendMetadata = () => {
+    this.props.metadataCallback(this.state.selectedMetadata)
+  }
+
+  render() {
+    const metadata = CLIN_METADATA
+
+    const metadataList = []
+    var item;
+    for (item of metadata) {
+      metadataList.push(
+        <DropdownItem
+          default onClick={this.handleClick}
+          key={item}>
+          {item}
+        </DropdownItem>)
+    }
+    return (
+      <ButtonDropdown direction="down" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          {this.state.selectedMetadata}
+        </DropdownToggle>
+        <DropdownMenu>
+          {metadataList}
+        </DropdownMenu>
+      </ButtonDropdown>
+    );
+
+  }
+};
+
+export default ClinMetadataDropdown;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,4 +5,25 @@
  // API URL where the Dashboard get all the data
 const BASE_URL = "http://ga4ghdev01.bcgsc.ca:20127";
 
+export const CLIN_METADATA = [
+    "celltransplants",
+    "chemotherapies",
+    "complications",
+    "consents",
+    "diagnoses",
+    "enrollments",
+    "immunotherapies",
+    "labtests",
+    "outcomes",
+    "patients",
+    "radiotherapies",
+    "samples",
+    "slides",
+    "studies",
+    "surgeries",
+    "treatments",
+    "tumourboards"
+]
+
 export default BASE_URL;
+
