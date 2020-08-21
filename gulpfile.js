d2ee34f39af88120964441b606b51ae1e5539609
haoyuanli <jimli@bcgsc.ca>
2020-08-21 09:37:37 -0700

    Update license (#40)
    * update license
    
    * update license

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,19 @@ const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
 gulp.task("licenses", async function () {
-  // this is to add Creative Tim licenses in the production mode for the minified js
+  // this is to add license in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
     .pipe(
       gap.prependText(`/*!
 
 =========================================================
-* Paper Dashboard React - v1.2.0
+* CanDIG V2 Dashboard - v0.1.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
+* Homepage: https://github.com/CanDIG/candigv2_dashboard
+* Copyright 2020 CanDIG Team
+* Copyright 2020 Creative Tim
 
 =========================================================
 
@@ -25,20 +24,19 @@ gulp.task("licenses", async function () {
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
-  // this is to add Creative Tim licenses in the production mode for the minified html
+  // this is to add license in the production mode for the minified html
   gulp
     .src("build/index.html", { base: "./" })
     .pipe(
       gap.prependText(`<!--
 
 =========================================================
-* Paper Dashboard React - v1.2.0
+* CanDIG V2 Dashboard - v0.1.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
+* Homepage: https://github.com/CanDIG/candigv2_dashboard
+* Copyright 2020 CanDIG Team
+* Copyright 2020 Creative Tim
 
 =========================================================
 
@@ -48,20 +46,19 @@ gulp.task("licenses", async function () {
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
-  // this is to add Creative Tim licenses in the production mode for the minified css
+  // this is to add license in the production mode for the minified css
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
     .pipe(
       gap.prependText(`/*!
 
 =========================================================
-* Paper Dashboard React - v1.2.0
+* CanDIG V2 Dashboard - v0.1.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
+* Homepage: https://github.com/CanDIG/candigv2_dashboard
+* Copyright 2020 CanDIG Team
+* Copyright 2020 Creative Tim
 
 =========================================================
 
