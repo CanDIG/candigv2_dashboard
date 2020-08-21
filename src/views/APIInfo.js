d1857cdde890a7c05880b39c10cf314ab1424286
Jimmy Li <lihaoyuan1996@gmail.com>
2020-08-21 11:26:14 -0700

    support multiple api yaml

--- src/views/APIInfo.js
+++ src/views/APIInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
@@ -7,7 +7,7 @@ import '../assets/css/APIInfo.css';
 
 // reactstrap components
 import {
-  Card, CardBody, Row, Col, Table,
+  Card, CardBody, Row, Col, Table, Input,
 } from 'reactstrap';
 
 // Consts
@@ -17,15 +17,38 @@ import BASE_URL from '../constants/constants';
  * API Info component
  */
 function APIInfo() {
+  const [apiDef, setApiDef] = useState(`${BASE_URL}/static/api.yaml`);
+  const apiDefinitionObj = {
+    'CanDIG-Server V1 API': `${BASE_URL}/static/api.yaml`,
+    'CanDIG-Server V1 Search & Count API': `${BASE_URL}/static/search_count_service.yaml`,
+    'CHORD Metadata Service': `${BASE_URL}/static/chord_metadata_service.yaml`,
+  };
+  const apiList = Object.keys(apiDefinitionObj).map((x) => (
+    <option key={x} value={x}>
+      {x}
+    </option>
+  ));
+
   return (
     <>
       <div className="content">
+
+        <Input
+          style={{ marginBottom: '10px' }}
+          onChange={(e) => {
+            setApiDef(apiDefinitionObj[e.currentTarget.value]);
+          }}
+          type="select"
+        >
+          { apiList }
+        </Input>
+
         <Row>
           <Col md="12">
             <Card>
               <CardBody className="all-icons">
                 <Table responsive>
-                  <SwaggerUI url={`${BASE_URL}/static/api.yaml`} />
+                  <SwaggerUI url={apiDef} />
                 </Table>
               </CardBody>
             </Card>
