4ce79b8ebee2a764520a401cd528efd0ab9a38e8
Felipe <fcoralsasso@bcgsc.ca>
2020-08-27 16:44:57 -0700

    Added new charts

--- src/views/IndividualsOverview.js
+++ src/views/IndividualsOverview.js
@@ -1,42 +1,129 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from 'react';
 // reactstrap components
-import { Card, CardBody, CardTitle, Row, Col } from "reactstrap";
+import {
+  Card, CardBody, CardTitle, Row, Col,
+} from 'reactstrap';
 
-import { CHORD_METADATA_URL } from "../constants/constants";
+import CustomOfflineChart from '../components/Graphs/CustomOfflineChart';
+import BoxPlotChart from '../components/Graphs/BoxPlotChart';
+
+import { CHORD_METADATA_URL } from '../constants/constants';
+
+function groupBy(objectArray, property) {
+  return objectArray.reduce((acc, obj) => {
+    const key = obj[property].charAt(0).toUpperCase()
+      + obj[property].slice(1).toLowerCase().replace('_', ' ');
+    if (!acc[key]) {
+      acc[key] = 0;
+    }
+    acc[key] += 1;
+    delete acc.undefined;
+    return acc;
+  }, {});
+}
+
+function groupExtraPropertieByGender(data, property) {
+  const extraPropertieList = {};
+  for (let i = 0; i < data.results.length; i += 1) {
+    const key = data.results[i].sex.charAt(0).toUpperCase()
+      + data.results[i].sex.slice(1).toLowerCase().replace('_', ' ');
+    if (!extraPropertieList[key]) {
+      extraPropertieList[key] = [];
+    }
+    extraPropertieList[key].push(
+      parseFloat(data.results[i].extra_properties[property]),
+    );
+  }
+  return extraPropertieList;
+}
+
+function countDiseases(data) {
+  const diseases = {};
+  for (let i = 0; i < data.results.length; i += 1) {
+    for (let j = 0; j < data.results[i].phenopackets.length; j += 1) {
+      for (
+        let k = 0;
+        k < data.results[i].phenopackets[j].diseases.length;
+        k += 1
+      ) {
+        const key = data.results[i].phenopackets[j].diseases[k].term.label;
+        if (!diseases[key]) {
+          diseases[key] = 0;
+        }
+        diseases[key] += 1;
+      }
+    }
+  }
+  return diseases;
+}
+
+function getCounterUnderExtraProperties(data, property) {
+  const education = {};
+  for (let i = 0; i < data.results.length; i += 1) {
+    const key = data.results[i].extra_properties[property];
+    if (!education[key]) {
+      education[key] = 0;
+    }
+    education[key] += 1;
+  }
+
+  return education;
+}
 
 function IndividualsOverview() {
-  const [individualsInfo, setIndividualsInfo] = useState({});
   const [individualCounter, setIndividualCount] = useState(0);
+  const [ethnicityObject, setEthnicityObject] = useState({ '': 0 });
+  const [genderObject, setGenderObject] = useState({ '': 0 });
+  const [doBObject, setDoBObject] = useState({ '': 0 });
+  const [diseasesObject, setDiseasesObject] = useState({ '': 0 });
+  const [educationObject, setEducationObject] = useState({ '': 0 });
+  const [boxPlotObject, setBoxPlotObject] = useState({ '': [] });
+  const [didFetch, setDidFetch] = useState(false);
 
-  const fetchData = () => {
-    fetch(CHORD_METADATA_URL + "/api/individuals?page_size=10000")
+  const countIndividuals = (data) => {
+    setIndividualCount(data.results.length);
+  };
+
+  const countEthnicity = (data) => {
+    setEthnicityObject(groupBy(data.results, 'ethnicity'));
+  };
+
+  const countGender = (data) => {
+    setGenderObject(groupBy(data.results, 'sex'));
+  };
+
+  const countDateOfBirth = (data) => {
+    setDoBObject(groupBy(data.results, 'date_of_birth'));
+  };
+
+  useEffect(() => {
+    fetch(`${CHORD_METADATA_URL}/api/individuals?page_size=10000`)
       .then((response) => response.json())
       .then((data) => {
-        // setIndividualsInfo(data);
-        console.log(data)
-        setIndividualCount(data.results.length);
+        countIndividuals(data);
+        countEthnicity(data);
+        countGender(data);
+        countDateOfBirth(data);
+        setDiseasesObject(countDiseases(data));
+        setEducationObject(getCounterUnderExtraProperties(data, 'education'));
+        setBoxPlotObject(groupExtraPropertieByGender(data, 'weight'));
+        setDidFetch(true);
       });
-  };
+  }, [didFetch]);
 
-  const countIndividuals = () => {
-    
-  };
-  // sex, ethnicity
-  // agregation of labeling under diseases
+  // sex, ethnicity - Ok
+  // agregation of labeling under diseases -ok
   // karyotypic_sex
-  // date_of_birth
+  // date_of_birth - ok
   // extra_properties -> height
   // extra_properties -> weight
   // extra_properties -> education
-
-  fetchData();
 
   return (
     <>
       <div className="content">
         <Row>
-          <Col lg="6" md="6" sm="6">
+          <Col lg="12" md="12" sm="12">
             <Card className="card-stats">
               <CardBody>
                 <Row>
@@ -53,6 +140,87 @@ function IndividualsOverview() {
                     </div>
                   </Col>
                 </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="6" md="6" sm="6">
+            <Card>
+              <CardBody>
+                <BoxPlotChart
+                  chartTitle="Weight"
+                  plotObject={boxPlotObject}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="6" md="12" sm="12">
+            <Card>
+              <CardBody>
+                <CustomOfflineChart
+                  datasetName=""
+                  dataObject={genderObject}
+                  chartType="pie"
+                  barTitle="Gender"
+                  height="400px; auto"
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="6" md="12" sm="12">
+            <Card>
+              <CardBody>
+                <CustomOfflineChart
+                  datasetName=""
+                  dataObject={ethnicityObject}
+                  chartType="bar"
+                  barTitle="Ethnicity"
+                  height="400px; auto"
+                />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="6" md="12" sm="12">
+            <Card>
+              <CardBody>
+                <CustomOfflineChart
+                  datasetName=""
+                  dataObject={diseasesObject}
+                  chartType="pie"
+                  barTitle="Diseases"
+                  height="400px; auto"
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="6" md="12" sm="12">
+            <Card>
+              <CardBody>
+                <CustomOfflineChart
+                  datasetName=""
+                  dataObject={doBObject}
+                  chartType="pie"
+                  barTitle="Date of Birth"
+                  height="400px; auto"
+                />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="6" md="12" sm="12">
+            <Card>
+              <CardBody>
+                <CustomOfflineChart
+                  datasetName=""
+                  dataObject={educationObject}
+                  chartType="bar"
+                  barTitle="Education"
+                  height="400px; auto"
+                />
               </CardBody>
             </Card>
           </Col>
