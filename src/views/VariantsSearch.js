4c72ce9147f060134577872d563e5ea350e66372
Jimmy Li <lihaoyuan1996@gmail.com>
2020-08-28 10:41:51 -0700

    update

--- src/views/VariantsSearch.js
+++ src/views/VariantsSearch.js
@@ -1,193 +1,190 @@
-import React, { useState }from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+  Button, Form, FormGroup, Label, Input,
+} from 'reactstrap';
 
 import { AgGridReact } from 'ag-grid-react';
+
+import BASE_URL, { CHORD_METADATA_URL } from '../constants/constants';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-import BASE_URL from 'constants/constants';
-import { CHORD_METADATA_URL } from 'constants/constants';
-
 function VariantsSearch({ datasetId }) {
+  const columnDefs = [
+    { headerName: 'Reference Name', field: 'referenceName' },
+    { headerName: 'Start', field: 'start' },
+    { headerName: 'End', field: 'end' },
+    { headerName: 'Reference Bases', field: 'referenceBases' },
+    { headerName: 'Alternate Bases', field: 'alternateBases' },
+  ];
 
-    let chord_data_cache = []
-    let retrieved = false;
+  const individualsColumnDefs = [
+    { headerName: 'ID', field: 'id' },
+    { headerName: 'Sex', field: 'sex' },
+    { headerName: 'Ethnicity', field: 'ethnicity' },
+    { headerName: 'Karyotypic Sex', field: 'karyotypic_sex' },
+    { headerName: 'Height', field: 'height' },
+    { headerName: 'Weight', field: 'weight' },
+    { headerName: 'Education', field: 'education' },
+  ];
 
-    if (retrieved === false) {
-        fetch(`${CHORD_METADATA_URL}/api/individuals?page_size=100`)
-        .then(response => response.json())
-        .then((data) => {
-            chord_data_cache = data["results"];
-        });
+  const [individualsRowData, setIndividualsRowData] = useState([]);
+  const [rowData, setRowData] = useState([]);
+  let gridOptions = {};
 
-        retrieved = true
+  const extraFieldHandler = (results) => {
+    const processedResults = [];
+
+    if (results === undefined) {
+      return [];
     }
 
+    for (let i = 0; i < results.length; i += 1) {
+      const tempObj = results[i];
+      tempObj.height = results[i].extra_properties.height;
+      tempObj.weight = results[i].extra_properties.height;
+      tempObj.education = results[i].extra_properties.education;
 
-    const columnDefs = [
-        { headerName: "referenceName", field: "referenceName" },
-        { headerName: "start", field: "start" },
-        { headerName: "end", field: "end" },
-        { headerName: "referenceBases", field: "referenceBases" },
-        { headerName: "alternateBases", field: "alternateBases" }
-    ];
+      processedResults.push(tempObj);
+    }
 
-    const individualsColumnDefs= [
-        { headerName: "ID", field: "id" },
-        { headerName: "Sex", field: "sex" },
-        { headerName: "Ethnicity", field: "ethnicity" },
-    ];
+    return processedResults;
+  };
 
-    const [individualsRowData, setIndividualsRowData] = useState([])
+  function onSelectionChanged() {
+    const selectedRows = gridOptions.api.getSelectedRows();
 
-    const [rowData, setRowData] = useState([]);
-    const gridOptions = {
-        defaultColDef: {
-          editable: false,
-          enableRowGroup: true,
-          enablePivot: true,
-          enableValue: true,
-          sortable: true,
-          resizable: true,
-          filter: true,
-          flex: 1,
-          minWidth: 20,
-          minHeight: 300
+    fetch(`${BASE_URL}/search`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        {
+          datasetId: gridOptions.context.datasetId,
+          logic: {
+            id: 'A',
+          },
+          components: [
+            {
+              id: 'A',
+              variants: {
+                start: selectedRows[0].start,
+                end: selectedRows[0].end,
+                referenceName: selectedRows[0].referenceName,
+              },
+            },
+          ],
+          results: [
+            {
+              table: 'patients',
+              fields: ['patientId'],
+            },
+          ],
         },
-        debug: true,
-        onSelectionChanged: onSelectionChanged,
-        rowSelection: 'single',
-        rowData: null,
-        rowGroupPanelShow: 'always',
-        pivotPanelShow: 'always',
-        enableRangeSelection: true,
-        paginationAutoPageSize: false,
-        pagination: true
-      };
+      ),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results !== undefined) {
+          let patientParams = '';
 
-    function onSelectionChanged() {
-        var selectedRows = gridOptions.api.getSelectedRows();
+          for (let i = 0; i < data.results.patients.length; i += 1) {
+            patientParams += `id=${data.results.patients[i].patientId}&`;
+          }
 
-        console.log(datasetId)
-
-        fetch(`${BASE_URL}/search`, {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(
-                {
-                    "datasetId": "WyJjYW5jb2dlbiJd",
-                    "logic": {
-                        "id": "A"
-                    },
-                    "components": [
-                        {
-                            "id": "A",
-                            "variants": {
-                                "start": selectedRows[0].start,
-                                "end": selectedRows[0].end,
-                                "referenceName": selectedRows[0].referenceName
-                            }
-                        }
-                    ],
-                    "results": [
-                        {
-                            "table": "patients"
-                        }
-                    ]
-                })
-          })
+          fetch(`${CHORD_METADATA_URL}/api/individuals?${patientParams}`)
             .then((response) => response.json())
-            .then((data) => {
-                console.log(data)
-                if (data.results !== undefined){
-
-                    let patientList = []
-                    let resultList = []
-
-                    for (let i = 0; i < data.results["patients"].length; i++){
-                        patientList.push(data.results["patients"][i]["patientId"])
-                    }
-
-                    for (let i = 0; i < chord_data_cache.length; i++) {
-                        if(patientList.includes(chord_data_cache[i]["id"])) {
-                            resultList.push(chord_data_cache[i]);
-                        }
-                    }
-
-                    console.log(patientList)
-                    console.log(resultList)
-                    console.log(chord_data_cache)
-
-                    setIndividualsRowData(resultList);
-                }
+            .then((chordData) => {
+              setIndividualsRowData(extraFieldHandler(chordData.results));
             });
-    }
-    
-    function formHandler(e) {
-        e.preventDefault(); // Prevent form submission
+        }
+      });
+  }
 
-        fetch(`${BASE_URL}/variants/search`, {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              start: e.target.start.value,
-              end: e.target.end.value,
-              referenceName: e.target.referenceName.value,
-              datasetId: datasetId
-            }),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.results !== undefined){
-                    setRowData(data.results["variants"]);
-                }
-            });
-    }
+  const formHandler = (e) => {
+    e.preventDefault(); // Prevent form submission
 
-    return (
-        <>
-        <div className="content">
+    fetch(`${BASE_URL}/variants/search`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        start: e.target.start.value,
+        end: e.target.end.value,
+        referenceName: e.target.referenceName.value,
+        datasetId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results !== undefined) {
+          setRowData(data.results.variants);
+        }
+      });
+  };
 
-            <Form onSubmit={formHandler}>
-                <FormGroup>
-                    <Label for="start">Start</Label>
-                    <Input required type="text" name="start" id="start" placeholder="Start location of a variant." />
-                </FormGroup>
+  gridOptions = {
+    defaultColDef: {
+      editable: false,
+      sortable: true,
+      resizable: true,
+      filter: true,
+      flex: 1,
+      minWidth: 20,
+      minHeight: 300,
+    },
+    debug: true,
+    onSelectionChanged,
+    rowSelection: 'single',
+    rowData: null,
+    rowGroupPanelShow: 'always',
+    pivotPanelShow: 'always',
+    enableRangeSelection: true,
+    paginationAutoPageSize: false,
+    pagination: true,
+  };
 
-                <FormGroup>
-                    <Label for="end">End</Label>
-                    <Input required type="text" name="end" id="end" placeholder="End location of a variant." />
-                </FormGroup>
+  return (
+    <>
+      <div className="content">
 
-                <FormGroup>
-                    <Label for="referenceName">Reference Name</Label>
-                    <Input required type="text" name="referenceName" id="referenceName" placeholder="Reference Name. This includes
-                    somatic and sex chromosomes, as well as mitochondria identifiers." />
-                </FormGroup>
+        <Form inline onSubmit={formHandler}>
+          <FormGroup style={{ marginRight: '20px' }}>
+            <Label style={{ marginRight: '10px' }} for="start">Start</Label>
+            <Input required type="text" name="start" id="start" placeholder="" />
+          </FormGroup>
 
-                <Button>Search</Button>
-            </Form>
+          <FormGroup style={{ marginRight: '20px' }}>
+            <Label style={{ marginRight: '10px' }} for="end">End</Label>
+            <Input required type="text" name="end" id="end" placeholder="" />
+          </FormGroup>
 
-            <div className="ag-theme-alpine" style={ {height: '400px', width: '100%'} }>
-                <AgGridReact
-                    columnDefs={columnDefs}
-                    rowData={rowData}
-                    gridOptions={gridOptions}>
-                </AgGridReact>
-            </div>
+          <FormGroup style={{ marginRight: '20px' }}>
+            <Label style={{ marginRight: '10px' }} for="referenceName">Reference Name</Label>
+            <Input required type="text" name="referenceName" id="referenceName" placeholder="" />
+          </FormGroup>
 
-            <div className="ag-theme-alpine" style={ {height: '400px', width: '100%', marginTop: '50px'} }>
-                <AgGridReact
-                    columnDefs={individualsColumnDefs}
-                    rowData={individualsRowData}>
-                </AgGridReact>
-            </div>
+          <Button>Search</Button>
+        </Form>
 
+        <div className="ag-theme-alpine" style={{ height: '400px', width: '100%', marginTop: '20px' }}>
+          <AgGridReact
+            columnDefs={columnDefs}
+            rowData={rowData}
+            gridOptions={gridOptions}
+            context={{ datasetId }}
+          />
         </div>
-        </>
-    )
-}
 
+        <div className="ag-theme-alpine" style={{ height: '400px', width: '100%', marginTop: '50px' }}>
+          <AgGridReact
+            columnDefs={individualsColumnDefs}
+            rowData={individualsRowData}
+          />
+        </div>
+
+      </div>
+    </>
+  );
+}
 
 export default VariantsSearch;
