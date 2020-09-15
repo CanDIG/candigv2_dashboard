19765f1f66942d842b0f56bdac1a4f40fc9d00f2
Dashaylan Naidoo <dashaylan@gmail.com>
2020-09-15 12:05:37 -0700

    Add loading indicator to Clincal Metadata

--- src/views/MetadataTableApp.jsx
+++ src/views/MetadataTableApp.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import BASE_URL from '../constants/constants';
 import ClinMetadataTable from '../components/Tables/ClinMetadataTable';
+import { trackPromise, usePromiseTracker } from 'react-promise-tracker';
+import LoadingIndicator from '../components/LoadingIndicator/LoadingIndicator';
 
 function CreateColumns(columnNames, cb) {
   const columnList = [];
@@ -50,33 +52,65 @@ function TableApp({ datasetId }) {
   const [data, setData] = useState([]);
   const [columns, setColumns] = useState([]);
 
+  const { promiseInProgress } = usePromiseTracker();
+
+
   React.useEffect(() => {
     // fetch data
     try {
-      getMetadataData(datasetId, selectedMetadata, setData);
+      if (datasetId) {
+        trackPromise(
+          fetch(`${BASE_URL}/${metadata}/search`, {
+            method: 'POST',
+            body: JSON.stringify({ datasetId }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+            .then((response) => {
+              if (response.ok) {
+                return response.json();
+              }
+              return {};
+            })
+            .then((data) => {
+              for (let i = 0; i < data.results[metadata].length; i += 1) {
+                datasets.push(data.results[metadata][i]);
+              }
+              setData(datasets);
+              CreateColumns(Object.keys(datasets[0]), setColumns);
+            })
+          );
+      }
     } catch (err) {
       // Need better reporting
       console.log(err);
     }
   }, [selectedMetadata, datasetId]);
 
-  React.useEffect(() => {
-    // Separate Effect since state change is async and columns depends on data
-    // Not entirely sure if necessary
-    try {
-      CreateColumns(Object.keys(data[0]), setColumns);
-    } catch (err) {
-      // Need better reporting
-      console.log(err);
-    }
-  }, [data]);
+  // React.useEffect(() => {
+  //   // Separate Effect since state change is async and columns depends on data
+  //   // Not entirely sure if necessary
+  //   try {
+  //     CreateColumns(Object.keys(data[0]), setColumns);
+  //   } catch (err) {
+  //     // Need better reporting
+  //     console.log(err);
+  //   }
+  // }, [data]);
 
   const dataM = React.useMemo(() => data, [data]);
   const columnsM = React.useMemo(() => columns, [columns]);
 
   return (
     <div className="content">
-      <ClinMetadataTable columns={columnsM} data={dataM} metadataCallback={setSelectedMetadata} />
+      {promiseInProgress === true ? (
+        <LoadingIndicator />
+        ) : (
+        <>
+        <ClinMetadataTable columns={columnsM} data={dataM} metadataCallback={setSelectedMetadata} />
+        </>
+      )}
     </div>
   );
 }
